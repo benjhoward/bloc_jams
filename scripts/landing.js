e8e4e97dfd98f228a5ffa192cf9d1134f62a7787
Ben <benjhoward412@gmail.com>
2017-03-06 21:53:13 -0500

    Checkpoint 8 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
-var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
- 
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) { 
      for (var i = 0; i < points.length; i++){
      var revealPoint = function() {
          points[i].style.opacity = 1;
@@ -13,38 +12,35 @@ var animatePoints = function() {
 }
 };
 
+window.onload = function() {
+    
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    addEventListener('scroll', function(event){
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
 
 
-
-// var animatePoints = function() {
+//var animatePoints = function() {
 // 
-//     var points = document.getElementsByClassName('point');
-// 
-//     var revealFirstPoint = function() {
-//         points[0].style.opacity = 1;
-//         points[0].style.transform = "scaleX(1) translateY(0)";
-//         points[0].style.msTransform = "scaleX(1) translateY(0)";
-//         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-//     };
-// 
-//     var revealSecondPoint = function() {
-//         points[1].style.opacity = 1;
-//         points[1].style.transform = "scaleX(1) translateY(0)";
-//         points[1].style.msTransform = "scaleX(1) translateY(0)";
-//         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-//     };
-// 
-//     var revealThirdPoint = function() {
-//         points[2].style.opacity = 1;
-//         points[2].style.transform = "scaleX(1) translateY(0)";
-//         points[2].style.msTransform = "scaleX(1) translateY(0)";
-//         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-//     };
-// 
-//     revealFirstPoint();
-//     revealSecondPoint();
-//     revealThirdPoint();
-// 
-// };
-
-//animatePoints();
+//    var points = document.getElementsByClassName('point');
+//    
+//    var revealPoint = function(index) {
+//        points[index].style.opacity = 1;
+//        points[index].style.transform = "rotate(0deg) translateY(0)";
+//        points[index].style.msTransform = "rotate(0deg) translateY(0)";
+//        points[index].style.WebkitTransform = "rotate(0deg) translateY(0)";
+//    }
+//    
+//    for (var i = 0; i < points.length; i++) {
+//        revealPoint(i);
+//    }
+//};
